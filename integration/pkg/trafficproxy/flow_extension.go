049b70bfff80e8356e994f681afaae3841754c4c
Jay Zhang <wangyoucao577@gmail.com>
2020-01-19 13:52:53 +0800

    feat: only use TrafficLevel_CLOSED to determine whether the flow blocked or not.

--- integration/pkg/trafficproxy/flow_extension.go
+++ integration/pkg/trafficproxy/flow_extension.go
@@ -2,13 +2,11 @@ package trafficproxy
 
 import "fmt"
 
-const blockingSpeedThreshold = 1 // Think it's blocking if flow speed smaller than this threshold.
-
 // IsBlocking tests whether the Flow is blocking or not.
 //   This function extends protoc-gen-go generated code on testing whether is blocking for Flow.
 func (f *Flow) IsBlocking() bool {
 
-	return f.TrafficLevel == TrafficLevel_CLOSED || f.Speed < blockingSpeedThreshold
+	return f.TrafficLevel == TrafficLevel_CLOSED
 }
 
 // CSVString represents Flow as defined CSV format.
