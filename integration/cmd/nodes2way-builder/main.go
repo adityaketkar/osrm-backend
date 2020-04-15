ead2c7d9f3975ce57f1fb98e9e426c2d4ffd63ea
Jay Zhang <wangyoucao577@gmail.com>
2020-04-15 05:41:44 +0000

    fix: log

--- integration/cmd/nodes2way-builder/main.go
+++ integration/cmd/nodes2way-builder/main.go
@@ -13,11 +13,12 @@ func main() {
 	defer glog.Flush()
 
 	startTime := time.Now()
-	defer glog.Infof("%s totally takes %f seconds for processing.", os.Args[0], time.Now().Sub(startTime).Seconds())
 
 	err := pipeline(flags.in, flags.out, flags.snappyCompressed)
 	if err != nil {
 		glog.Error(err)
 		os.Exit(1)
 	}
+
+	glog.Infof("%s totally takes %f seconds for processing.", os.Args[0], time.Now().Sub(startTime).Seconds())
 }
