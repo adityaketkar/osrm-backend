85f1a4fd0c6e69b430d5acd361cda0b933d47890
Xun(Perry) Liu <coderxiong@gmail.com>
2020-07-28 13:18:43 -0700

    feat: Create script in dockerfile to produce OASIS data  (#362)
    * feat: implements docker file for building data for oasis service
    issue: https://github.com/Telenav/osrm-backend/issues/361

--- docker-orchestration/osrm-backend/README.md
+++ docker-orchestration/osrm-backend/README.md
@@ -81,6 +81,44 @@ $ docker logs -f 5b54931c035abaa0d0635cae4539da91e91fca02d1b37426451aa73476dd53f
 [info] running and waiting for requests
 ```
 
-## Example By Manual
-- [Build Berlin Server with OSM data](./example-berlin-osm.md)
+### Oasisd service
 
+Image builds `osrm-api-oasisd` binaries, including generating pre-processing data by `place-connectivity-gen`, or start web service of `oasisd`.  
+
+#### Build data
+
+`docker run [EXTRA COMMAND] telenavmap/osrm-api-oasisd build_oasis_from_json [JSON_FILE]`  
+
+##### Parameters
+
+- Mandatory parameters  
+  - JSON_FILE: could be http url or local file  
+- Optional commands  
+  - `--mount "type=bind,src=$(pwd),dst=/workspace/mnt"`: mount for local PBF file input or built data output  
+  - `-osrm OSRM_ENDPOINT`: osrm endpoint to calculate shortest path, if not assigned then with use great circle distance
+
+##### Samples
+
+```bash
+$ pwd
+/github.com/Telenav/osrm-backend/docker-orchestration/osrm-api-oasisd
+
+$ ls -lk
+-rw-r--r--  1 ngxuser  staff       163 Jul 20 15:21 Dockerfile
+-rw-r--r--  1 ngxuser  staff       319 Jul 20 15:21 README.md
+-rwxr-xr-x  1 ngxuser  staff      1583 Jul 20 15:21 docker-entrypoint.sh
+-rw-------@ 1 ngxuser  staff  18403706 Mar 30 09:15 us.json
+
+$ docker run --mount "type=bind,src=$(pwd),dst=/workspace/mnt" osrm-api-oasisd:latest build_oasis_from_json -i us.json
+
+$ ls -lk oasisdata
+total 6812660
+-rw-r--r--  1 ngxuser  staff     3532902 Jul 20 15:40 cellID2PointIDs.gob
+-rw-r--r--  1 ngxuser  staff         428 Jul 20 15:44 connectivity_map_statistic.json
+-rw-r--r--  1 ngxuser  staff  6956466286 Jul 20 15:44 id2nearbyidsmap.gob
+-rw-r--r--  1 ngxuser  staff      639266 Jul 20 15:40 pointID2Location.gob
+```
+
+## Example By Manual
+
+- [Build Berlin Server with OSM data](./example-berlin-osm.md)
--- docker-orchestration/osrm-backend/docker-entrypoint.sh
+++ docker-orchestration/osrm-backend/docker-entrypoint.sh
@@ -8,6 +8,9 @@ PBF_FILE_SUFFIX=".osm.pbf"
 SNAPPY_SUFFIX=".snappy"
 WAYID2NODEIDS_MAPPING_FILE=wayid2nodeids.csv
 NODES2WAY_DB_FILE="nodes2way.db"
+INPUT_STATION_DATA_JSON_FORMAT=input.json
+MOUNT_PATH=/workspace/mnt # mounted host folder
+OASIS_DATA_RELATIVE_PATH="oasis-data"
 
 _sig() {
   kill -TERM $child 2>/dev/null
@@ -87,6 +90,45 @@ elif [ "$1" = 'compile_mapdata' ]; then
   COMPILED_DATA_EXPORT_PATH=/compiled-data
   mv ${DATA_PATH}/* ${COMPILED_DATA_EXPORT_PATH}/
   chmod 777 ${COMPILED_DATA_EXPORT_PATH}/*
+
+elif [ "$1" = 'build_oasis_from_json' ]; then
+
+  # parse input parameters, should align with integration/cmd/place-connectivity-gen/flags.go
+  JSON_FILE=
+  OSRM_ENDPOINT=
+
+  # skip first parameter
+  shift 1
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      -i|--input) JSON_FILE="$2"; shift ;;
+      -osrm) OSRM_ENDPOINT="$2"; shift ;;
+      *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+  done
+
+  # create output folder
+  mkdir -p ${MOUNT_PATH}/${OASIS_DATA_RELATIVE_PATH}
+
+  # construct path for input
+  if [[ ${JSON_FILE} = http* ]]; then 
+    curl -sSL -f ${JSON_FILE} > ${DATA_PATH}/${INPUT_STATION_DATA_JSON_FORMAT}
+  else 
+    cp ${MOUNT_PATH}/${JSON_FILE} ${DATA_PATH}/${INPUT_STATION_DATA_JSON_FORMAT}
+  fi
+
+  # generate parameters for oasis preprocessing
+  OASIS_PREPROCESSING_PARAMETERS=
+  if [ -z "$osrm" ]
+  then
+    OASIS_PREPROCESSING_PARAMETERS="${OASIS_PREPROCESSING_PARAMETERS} -i ${DATA_PATH}/${INPUT_STATION_DATA_JSON_FORMAT} -o ${MOUNT_PATH}/${OASIS_DATA_RELATIVE_PATH}"
+  else
+    OASIS_PREPROCESSING_PARAMETERS="${OASIS_PREPROCESSING_PARAMETERS} -i ${DATA_PATH}/${INPUT_STATION_DATA_JSON_FORMAT} -o ${MOUNT_PATH}/${OASIS_DATA_RELATIVE_PATH} -osrm ${OSRM_ENDPOINT}"
+  fi
+
+  # generate data
+  ${BUILD_PATH}/place-connectivity-gen ${OASIS_PREPROCESSING_PARAMETERS}
 
 else
   exec "$@"
